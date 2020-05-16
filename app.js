932ed51545c6e266a4aab8650b9feb94e9a5ad2f
qcjn <40976058+qcjn@users.noreply.github.com>
2020-05-16 23:34:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.get("/", (req, res) => {
     .catch(error => res.status(422).json(error))
 })
 
-app.listen(80, () => console.log(`Example app listening on port 80!`))
+app.listen(8080, () => console.log(`Example app listening on port 8080!`))
