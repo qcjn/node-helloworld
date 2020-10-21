3d53231cd87a79029f8735508d93b2ac6334a653
qcjn <40976058+qcjn@users.noreply.github.com>
2020-10-22 07:39:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = '80'
+const port = 5000
 
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port)
